4bd8aeee9f153e72e9106aa930e590c7833d4c13
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-05-21 15:07:01 +0800

    Update Source.cpp

--- 6. Zigzag Conversion/Source.cpp
+++ 6. Zigzag Conversion/Source.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <numbers>
 
 using namespace std;
 
@@ -11,20 +12,20 @@ public:
 		auto get_row = [rho](const int p) {
 			const int Q = p / (rho - 1), R = p % (rho - 1);
 			switch (Q % 2) {
-			case 0: return R;
-			case 1: return rho - 1 - R;
+			case 0: return R;			// 字符 s[p] 位于锯齿形的竖直部分，不含竖直部分与倾斜部分的位于下侧的交点
+			case 1: return rho - 1 - R; // 字符 s[p] 位于锯齿形的倾斜部分，或竖直部分与倾斜部分的位于下侧的交点
 			}
 			return -1;
 		};
-		auto get_column = [rho, s](const int p) {
+		auto get_column = [rho](const int p) {
 			const int Q = p / (rho - 1);
 			switch (Q % 2) {
-			case 0: return Q / 2 * (rho - 1);
-			case 1: return p - (Q + 1) / 2 * (rho - 1);
+			case 0: return Q / 2 * (rho - 1);			// 字符 s[p] 位于锯齿形的竖直部分，不含竖直部分与倾斜部分的位于下侧的交点
+			case 1: return p - (Q + 1) / 2 * (rho - 1);	// 字符 s[p] 位于锯齿形的倾斜部分，或竖直部分与倾斜部分的位于下侧的交点
 			}
 			return -1;
 		};
-		vector<string> a;
+		vector<string> a; // 矩阵共有 rho 行 chi 列
 		const int chi = get_column(s.length() - 1) + 1;
 		for (int i = 0; i < rho; ++i) { a.emplace_back(chi, ' '); }
 		for (size_t p = 0; p < s.length(); ++p) {
