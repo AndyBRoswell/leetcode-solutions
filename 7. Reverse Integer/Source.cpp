ce58a54fbf67ee341841acb9f6b8ddcf0f27851c
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-15 22:34:16 +0200

    7

---
+++ 7. Reverse Integer/Source.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int reverse(int x) {
+        constexpr int t = INT_MAX / 10;
+        const int f = x < 0 ? -1 : 1;
+        x = abs(x);
+        div_t p{ x, 0 };
+        int a = 0;
+        while (true) {
+            p = div(p.quot, 10);
+            if (a > t) { return 0; }
+            else if (a == t && p.rem > 7) { return 0; }
+            a = a * 10 + p.rem;
+            if (p.quot == 0) { return f * a; }
+        }
+    }
+};
