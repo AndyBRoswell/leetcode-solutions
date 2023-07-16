a9d8259c31eb2b0e444a028d7eea6f6804673043
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-16 21:01:51 +0200

    Update Source.cpp

--- 10. Regular Expression Matching/Source.cpp
+++ 10. Regular Expression Matching/Source.cpp
@@ -7,34 +7,15 @@
 
 class Solution {
 public:
-    bool isMatch(const std::string& S, const std::string& P) {
-        std::vector<std::string_view> PT;
-        const std::string_view V(P);
-        for (size_t i = 0; i < P.size(); ++i) {
-            if (i + 1 < P.size() && P[i + 1] == '*') {
-                PT.emplace_back(V.substr(i, 2));
-                ++i;
-            }
-            else { PT.emplace_back(V.substr(i, 1)); }
-        }
-        size_t i = 0, j = 0;
-        while (i < S.size() && j < PT.size()) {
-            if (PT[j].find('*') != std::string_view::npos) { // backtracking is needed: https://learn.microsoft.com/en-us/dotnet/standard/base-types/backtracking-in-regular-expressions
-                if (PT[j][0] == '.') { // '.*'
-                    i = S.size(); // match all
-                    ++j;
-                }
-                else { // ...
-                    while (i < S.size() && S[i] == PT[j][0]) { ++i; } // match
-                    ++j;
-                }
-            }
-            else {
-                if (PT[j][0] == '.' || S[i] == PT[j][0]) { ++i; ++j; } // match
-                else { return false; }  // mismatch
-            }
-        }
-        return S.size() == i && PT.size() == j; // complete match is required
+    bool isMatch(const std::string& S, const std::string& P) { return has_match_1(S, P); }
+    bool has_match_1(const std::string& S, const std::string& P) {
+
+    }
+    bool has_match_2_top_down(const std::string& S, const std::string& P) {
+
+    }
+    bool has_match_2_bottom_up(const std::string& S, const std::string& P) {
+
     }
 };
 
