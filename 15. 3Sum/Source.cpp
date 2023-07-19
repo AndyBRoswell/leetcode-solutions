1cfdec42f184fddaa6ff14e8bf2f49e926c1f870
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-20 01:06:53 +0200

    16

--- 15. 3Sum/Source.cpp
+++ 15. 3Sum/Source.cpp
@@ -10,12 +10,12 @@ public:
             if (i != nums.cbegin() && *i == *(i - 1)) { continue; } // eliminate duplicates
             auto L = i + 1, R = nums.cend() - 1;
             while (L < R) {
-                auto sum = *i + *L + *R;
+                const int sum = *i + *L + *R;
                 if (sum == 0) {
                     result.push_back({ *i, *L, *R });
-                    while (L < R && L + 1 < nums.cend() && *L == *(L + 1)) { ++L; } // eliminate duplicates
-                    while (L < R && R - 1>nums.cbegin() && *R == *(R - 1)) { --R; } // eliminate duplicates
-                    ++L; --R;                                                       // only move one of the pointers is not enough after eliminating duplicates
+                    while (L < R && L + 1 < nums.cend() && *L == *(L + 1)) { ++L; }     // eliminate duplicates
+                    while (L < R && R - 1 > nums.cbegin() && *R == *(R - 1)) { --R; }   // eliminate duplicates
+                    ++L; --R;                                                           // only move one of the pointers is not enough after eliminating duplicates
                 }
                 else if (sum < 0) { ++L; }
                 else { --R; }
--- 16. 3Sum Closest/Source.cpp
+++ 16. 3Sum Closest/Source.cpp
@@ -1,12 +1,39 @@
 #include <vector>
 #include <algorithm>
+#include <array>
+#include <iostream>
 
 class Solution {
 public:
     int threeSumClosest(std::vector<int>& nums, int target) {
         std::sort(nums.begin(), nums.end());
+        int ans = nums[0] + nums[1] + nums[2];
         for (auto i = nums.cbegin(); i != nums.cend() - 2; ++i) {
-
+            if (i != nums.cbegin() && *i == *(i - 1)) { continue; } // eliminate duplicates
+            auto L = i + 1, R = nums.cend() - 1;
+            while (L < R) {
+                const int sum = *i + *L + *R;
+                if (abs(sum - target) < abs(ans - target)) {
+                    ans = sum;
+                    if (ans == target) { return ans; } // early return
+                    while (L < R && L + 1 < nums.cend() && *L == *(L + 1)) { ++L; }     // eliminate duplicates
+                    while (L < R && R - 1 > nums.cbegin() && *R == *(R - 1)) { --R; }   // eliminate duplicates
+                }
+                if (ans < target) { ++L; }
+                else { --R; }
+            }
         }
+        return ans;
     }
 };
+
+int main() {
+    Solution s;
+    struct input { std::vector<int> nums; int target; };
+    std::array<struct input, 1> inputs{ {
+        { {4, 0, 5, -5, 3, 3, 0, -4, -5}, -2 }
+        }};
+    for (auto& input : inputs) {
+        std::cout << s.threeSumClosest(input.nums, input.target) << std::endl;
+    }
+}
