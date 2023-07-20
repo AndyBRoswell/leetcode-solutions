f1ebbfae1e9014563c3cdc31a7020ab4696c4dfc
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-20 02:31:36 +0200

    Update Source.cpp

--- 17. Letter Combinations of a Phone Number/Source.cpp
+++ 17. Letter Combinations of a Phone Number/Source.cpp
@@ -1,23 +1,31 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <iostream>
 
 class Solution {
 public:
     std::vector<std::string> letterCombinations(std::string& digits) {
         if (digits.empty()) { return {}; }
         constexpr std::string_view L[] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-        for (auto& digit : digits) { digit = digit - '0'; } // just for convenience of subsequent processing
-        const std::set<int> s(digits.cbegin(), digits.cend());
-        const std::vector<int> d(s.cbegin(), s.cend());
+        for (auto& digit : digits) { digit = digit - '0'; }     // just for convenience of subsequent processing
+        const std::set<int> s(digits.cbegin(), digits.cend());  // optional: unique & sort digits
+        const std::vector<int> d(s.cbegin(), s.cend());         // d: digit
         std::vector<std::string> ans;
         const std::vector<size_t> v0(d.size(), 0);
         std::vector<size_t> v = v0;
         do {
-            for (size_t i = 0; i < L[*d.crbegin()].size(); ++i) {
+            for (size_t i = 0; i < L[*d.crbegin()].size(); ++i, ++ * v.rbegin()) {
                 std::string str;
                 for (size_t i = 0; i < d.size(); ++i) { str += L[d[i]][v[i]]; }
                 ans.push_back(str);
+            }
+            *v.rbegin() = 0;
+            if (d.size() > 1) { // carry
+                for (auto i = v.rbegin() + 1; i != v.rend(); ++i) {
+                    if (*i < L[d[i - v.rbegin()]].size()) { break; }
+                    *i = 0;
+                }
             }
         } while (v != v0);
         return ans;
@@ -25,5 +33,12 @@ public:
 };
 
 int main() {
-
+    Solution s;
+    constexpr const char* inputs[] = { "23", };
+    for (const auto& input : inputs) {
+        std::string digits(input);
+        const auto ans = s.letterCombinations(digits);
+        for (const auto& str : ans) { std::cout << str << ' '; }
+        std::cout << std::endl;
+    }
 }
