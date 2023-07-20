85fb8d89522780a7a8e6f3f2b0c89b43b5f7b284
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-20 02:36:59 +0200

    Update Source.cpp

--- 17. Letter Combinations of a Phone Number/Source.cpp
+++ 17. Letter Combinations of a Phone Number/Source.cpp
@@ -8,9 +8,8 @@ public:
     std::vector<std::string> letterCombinations(std::string& digits) {
         if (digits.empty()) { return {}; }
         constexpr std::string_view L[] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-        for (auto& digit : digits) { digit = digit - '0'; }     // just for convenience of subsequent processing
-        const std::set<int> s(digits.cbegin(), digits.cend());  // optional: unique & sort digits
-        const std::vector<int> d(s.cbegin(), s.cend());         // d: digit
+        for (auto& digit : digits) { digit = digit - '0'; }         // just for convenience of subsequent processing
+        const std::vector<int> d(digits.cbegin(), digits.cend());   // d: digit
         std::vector<std::string> ans;
         const std::vector<size_t> v0(d.size(), 0);
         std::vector<size_t> v = v0;
