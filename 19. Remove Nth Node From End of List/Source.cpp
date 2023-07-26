4ab4210567a71c883830eed0ccf3a7309abe1b0a
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-26 20:52:19 +0200

    Update Source.cpp

--- 19. Remove Nth Node From End of List/Source.cpp
+++ 19. Remove Nth Node From End of List/Source.cpp
@@ -1,11 +1,12 @@
+#include <iostream>
 
 // Definition for singly-linked list.
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class Solution {
@@ -14,14 +15,44 @@ public:
         ListNode* p = head;
         int N = 0;
         while (p->next != nullptr) { p = p->next; ++N; }
-        if (n == 1) {
-
+        ++N;
+        if (N == 1) { return nullptr; }
+        else if (N == 2) {
+            switch (n) {
+            case 1:
+                head->next = nullptr;
+                return head;
+            case 2:
+                return head->next;
+            }
         }
-        else if (n == N) {
-
+        else { // N >= 3
+            p = head;
+            if (n == 1) {
+                while (p->next->next != nullptr) { p = p->next; }
+                p->next = nullptr;
+                return head;
+            }
+            else if (n == N) {
+                return head->next;
+            }
+            else {
+                for (int i = 0; i < N - n - 1; ++i) { p = p->next; }
+                p->next = p->next->next;
+                return head;
+            }
         }
-        else {
-
-        }
+        return nullptr; // never reach here
     }
 };
+
+int main() {
+    Solution s;
+    ListNode* head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    ListNode* p = s.removeNthFromEnd(head, 2);
+    while (p != nullptr) {
+        std::cout << p->val << "->";
+        p = p->next;
+    }
+    return 0;
+}
