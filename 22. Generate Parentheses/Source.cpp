3093dcb575c58f6de326926fa3ae30664080d965
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-30 14:25:41 +0200

    Update Source.cpp

--- 22. Generate Parentheses/Source.cpp
+++ 22. Generate Parentheses/Source.cpp
@@ -3,13 +3,13 @@
 
 class Solution {
 public:
-    void DFS(int n, int nL, int nR, std::vector<std::string>& S, const std::string& s) {
-        if (s.size() == 2 * n) {
-            S.emplace_back(s);
+    void DFS(int n, int nL, int nR, std::vector<std::string>& S, const std::string& p) {
+        if (nL == n) {
+            S.emplace_back(p + std::string(n - nR, ')'));
             return;
         }
-        if (nL < n) { DFS(n, nL + 1, nR, S, s + '('); }
-        if (nR < nL) { DFS(n, nL, nR + 1, S, s + ')'); }
+        if (nL < n) { DFS(n, nL + 1, nR, S, p + '('); }
+        if (nR < nL) { DFS(n, nL, nR + 1, S, p + ')'); }
     }
     std::vector<std::string> generateParenthesis(int n) {
         std::vector<std::string> ans;
