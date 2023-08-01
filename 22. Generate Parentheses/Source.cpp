1fa4e6f40ee3ac5f941fef7ce3e3861bc286f4c3
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-08-01 23:20:28 +0200

    Update Source.cpp

--- 22. Generate Parentheses/Source.cpp
+++ 22. Generate Parentheses/Source.cpp
@@ -4,16 +4,16 @@
 class Solution {
 public:
     void DFS(int n, int nL, int nR, std::vector<std::string>& S, const std::string& p) {
-        if (nL == n) {
-            S.emplace_back(p + std::string(n - nR, ')'));
+        if (nL == n) { // reached the maximum recursion depth
+            S.emplace_back(p + std::string(n - nR, ')')); // only 1 possible solution
             return;
         }
-        if (nL < n) { DFS(n, nL + 1, nR, S, p + '('); }
-        if (nR < nL) { DFS(n, nL, nR + 1, S, p + ')'); }
+        if (nL < n) { DFS(n, nL + 1, nR, S, p + '('); }     // prefix p + '(' is possible
+        if (nR < nL) { DFS(n, nL, nR + 1, S, p + ')'); }    // prefix p + ')' is possible
     }
     std::vector<std::string> generateParenthesis(int n) {
         std::vector<std::string> ans;
-        DFS(n, 0, 0, ans, "");
+        DFS(n, 0, 0, ans, ""); // start with empty prefix
         return ans;
     }
 };
