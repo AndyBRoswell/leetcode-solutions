d008d0c5cac03ef4015ef392366a2d83a6c41794
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-08-11 21:34:28 +0200

    Update Source.cpp

--- 25. Reverse Nodes in k-Group/Source.cpp
+++ 25. Reverse Nodes in k-Group/Source.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 // Definition for singly-linked list.
 struct ListNode {
     int val;
@@ -9,7 +11,22 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
+    ListNode* reverseKGroup(ListNode* head, const int k) {
+        if (head == nullptr) { return head; }
+        ListNode* const dummy = new ListNode(0, head);
+        std::vector<ListNode*> p(k + 1);
+        auto try_next_nodes = [&]() {
+            for (int i = 0; i < k; ++i) {
+                if (p[i]->next != nullptr) { p[i + 1] = p[i]->next; }
+                else { return false; }
+            }
+            return true;
+            };
+        for (p[0] = dummy; p[0] != nullptr; p[0] = p[k - 1]->next) {
+            if (try_next_nodes() == false) { break; }
+            p[0]->next = p[k - 1]->next;
+            for (int i = k - 1; i > 0; --i) { p[i]->next = p[i - 1]; }
 
+        }
     }
 };
