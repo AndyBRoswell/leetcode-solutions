5dab7318fbb631f7f7f407c18282ba4aeb485a39
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-09-10 01:27:57 +0200

    Update Source.cpp

--- 30. Substring with Concatenation of All Words/Source.cpp
+++ 30. Substring with Concatenation of All Words/Source.cpp
@@ -5,47 +5,10 @@
 class Solution {
 public:
     std::vector<int> findSubstring(const std::string& s, const std::vector<std::string>& words) {
-        return find_substring_2(s, words);
+        return find_substring_1(s, words);
     }
+    // https://leetcode.com/problems/substring-with-concatenation-of-all-words/solutions/2419835/c-most-optimal-solution-beats-96-easy-readable-and-commented-code/
     std::vector<int> find_substring_1(const std::string& s, const std::vector<std::string>& words) {
-        std::vector<int> result;
-        const int n = s.length();
-        const int numWords = words.size();
-        if (n == 0 || numWords == 0) { return result; }
-
-        const int wordLen = words[0].length();
-        //int totalLen = numWords * wordLen;
-        std::unordered_map<std::string, int> wordCount; // Stores the count of each word
-
-        for (const std::string& word : words) { wordCount[word]++; }
-        for (int i = 0; i < wordLen; ++i) {
-            int left = i, right = i;
-            int count = 0; // the number of valid words in the current window
-            std::unordered_map<std::string, int> currentCount; // Stores count of words in current window
-            while (right + wordLen <= n) {
-                std::string currentWord = s.substr(right, wordLen);
-                right += wordLen;
-                if (wordCount.contains(currentWord)) {
-                    currentCount[currentWord]++;
-                    count++;
-                    while (currentCount[currentWord] > wordCount[currentWord]) {
-                        std::string leftWord = s.substr(left, wordLen);
-                        left += wordLen;
-                        currentCount[leftWord]--;
-                        count--;
-                    }
-                    if (count == numWords) { result.push_back(left); }
-                }
-                else {
-                    currentCount.clear();
-                    count = 0;
-                    left = right;
-                }
-            }
-        }
-        return result;
-    }
-    std::vector<int> find_substring_2(const std::string& s, const std::vector<std::string>& words) {
         std::vector<int> ans;
         std::unordered_map<std::string, int> required_occurrence_count; {
             for (auto& word : words) { required_occurrence_count[word]++; }
@@ -54,7 +17,7 @@ public:
         int L = 0, R = 0;                   // current window. Note: The step length of these 2 endpoints always equals to single word length.
         const int N = s.length();           // length of input string s
         std::unordered_map<std::string, int> occurrence_count_in_window;
-        // We only need to let the left endpoint of the window be in [0, word length) to get every possible combination
+        // We only need to let the left endpoint of the window be in [0, word length) to get every possible combination. The cases where the left endpoint is in [word length, n) will be covered by the iterations of the right endpoint.
         for (int i = 0; i < l; i++) {
             L = i, R = i; // At the beginning of each iteration, the window size is 0
             occurrence_count_in_window.clear();
@@ -68,34 +31,29 @@ public:
                     L = R;                              // The left endpoint of the window must be at the position of the next word
                     continue;
                 }
-                if (occurrence_count_in_window[word] < required_occurrence_count[word]) { occurrence_count_in_window[word]++, word_count_in_window++; }
-                else {
-                    while (L < R and occurrence_count_in_window[word] >= required_occurrence_count[word]) {
-                        auto word_to_remove = s.substr(L, l);
-                        occurrence_count_in_window[word_to_remove]--;
-                        if (occurrence_count_in_window[word_to_remove] == 0) { occurrence_count_in_window.erase(word_to_remove); }
-                        word_count_in_window--;
-                        L += l;
-                    }
-                    // Now the window has becomes valid so we can add the curr word to the usedWords
-                    occurrence_count_in_window[word]++, word_count_in_window++;
+                while (L < R and occurrence_count_in_window[word] >= required_occurrence_count[word]) { // The current word has already appeared more than the required times
+                    auto word_to_remove = s.substr(L, l); // Optimization available: Recording all the occurrence positions of each word in the words array
+                    occurrence_count_in_window[word_to_remove]--;
+                    if (occurrence_count_in_window[word_to_remove] == 0) { occurrence_count_in_window.erase(word_to_remove); }
+                    word_count_in_window--;
+                    L += l; // we need to shrink the window with the left endpoint, until the current word has the required occurrence count
                 }
-                R += l;
-                // if the words of our current window get equal to the total words required, then we can add the index of the starting of the window (left pointer) to our answer array, and move the left pointer 1 step forward.
-                // We can't set the left pointer to point to right because we might have an answer starting from left + word_len. So we only increment it 1 step forward.
+                occurrence_count_in_window[word]++, word_count_in_window++;
+                R += l; // expand the window by 1 word via the right endpoint
                 if (word_count_in_window == words.size()) { // Note: Here len[left, right) == total_words * word_len
                     ans.push_back(L);
                     auto word_to_remove = s.substr(L, l);
                     occurrence_count_in_window[word_to_remove]--;
                     if (occurrence_count_in_window[word_to_remove] == 0) { occurrence_count_in_window.erase(word_to_remove); }
                     word_count_in_window--;
-                    L += l;
+                    L += l; // shrink the window by 1 word via the left endpoint
                 }
             }
         }
         return ans;
     }
-    std::vector<int> find_substring_3(const std::string& s, const std::vector<std::string>& words) {
+    // TODO: Comment on this solution
+    std::vector<int> find_substring_2(const std::string& s, const std::vector<std::string>& words) {
         struct matcher {
             struct info { int mtindex, count; };
             std::unordered_map<std::string_view, info> dict;
